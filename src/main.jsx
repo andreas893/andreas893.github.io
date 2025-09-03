80495e5d3a56bc39de56f8677fd2772796608505
andreas893 <eaa24andrh@students.eaaa.dk>
2025-09-03 10:28:43 +0200

    ha

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter   basename={import.meta.env.DEV ? "/" : "andreas893.github.io"}>
       <App />
     </BrowserRouter>  
   </StrictMode>
